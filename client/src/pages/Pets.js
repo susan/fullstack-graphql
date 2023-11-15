61dee291864403325637983a5eecf85d198726bd
susan <susan@users.noreply.github.com>
2023-11-15 14:11:19 -0500

    pets.js for client

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,33 +1,33 @@
 import React, {useState} from 'react'
 import gql from 'graphql-tag'
-import PetBox from '../components/PetBox'
-import NewPet from '../components/NewPet'
 import { useQuery, useMutation } from '@apollo/react-hooks'
+import PetsList from '../components/PetsList'
+import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
+
 
 export default function Pets () {
   const [modal, setModal] = useState(false)
-  
+  const GET_PETS= gql `
+    query GetPets {
+      pets {
+        name
+        id
+        img
+      }
+     }
+   `
+
+  const {data, loading, error}  = useQuery(GET_PETS)
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
   const onSubmit = input => {
     setModal(false)
   }
-
-  const petsList = pets.data.pets.map(pet => (
-    <div className="col-xs-12 col-md-4 col" key={pet.id}>
-      <div className="box">
-        <PetBox pet={pet} />
-      </div>
-    </div>
-  ))
   
   if (modal) {
-    return (
-      <div className="row center-xs">
-        <div className="col-xs-8">
-          <NewPet onSubmit={onSubmit} onCancel={() => setModal(false)}/>
-        </div>
-      </div>
-    )
+    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
 
   return (
@@ -35,7 +35,7 @@ export default function Pets () {
       <section>
         <div className="row betwee-xs middle-xs">
           <div className="col-xs-10">
-            <h1>Pets</h1>
+            <h1>Pets </h1>
           </div>
 
           <div className="col-xs-2">
@@ -44,9 +44,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <div className="row">
-          {petsList}
-        </div>
+        <PetsList pets={data.pets} />
       </section>
     </div>
   )
